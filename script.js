13ddb4d41f73df6f113098e6862c2936f1355c8a
RitikaSrivastava2204 <158809116+RitikaSrivastava2204@users.noreply.github.com>
2024-02-08 23:27:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type",{
+    strings: ["a Web Developer"," an Intern @CodSoft"," a Learner","a Creative Coder", "a Singer"],
+    typeSpeed: 120,
+    backSpeed: 75,
+    loop: true
+})
